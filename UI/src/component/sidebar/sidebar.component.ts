0bd3a3cafaf3633eff417537ef5982ebe256c885
Anjana97-s <anjana.s@innovaturelabs.org>
2023-01-06 14:09:56 +0530

    mobile view

--- UI/src/component/sidebar/sidebar.component.ts
+++ UI/src/component/sidebar/sidebar.component.ts
@@ -5,11 +5,39 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit
+{
 
   constructor() { }
 
-  ngOnInit(): void {
+  private selector(el: string , all = false): any
+  {
+    el = el.trim();
+    if (all)
+    {
+      return Array.from(document.querySelectorAll(el));
+    }
+    else
+    {
+      return document.querySelector(el);
+    }
+  }
+
+
+
+  ngOnInit(): void
+  {
+
+  }
+
+  public mobileViewToggle(e): any
+  {
+    console.log(e);
+
+    const mobile  = this.selector('.mobile-nav-toggle')
+    this.selector('body').classList.toggle('mobile-nav-active')
+    mobile.classList.toggle('fa-bars')
+    mobile.classList.toggle('fa-xmark')
   }
 
 }
